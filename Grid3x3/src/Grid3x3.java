3769e1e74b3bf7b323bca1e0e5f4434fadc21ba9
Jack Margeson <margeson.jack@gmail.com>
2019-08-26 21:28:19 -0400

    Grid3x3 do while

--- Grid3x3/src/Grid3x3.java
+++ Grid3x3/src/Grid3x3.java
@@ -5,41 +5,52 @@ Jack Margeson, 08/21/2019 */
 // Importations
 import javax.swing.*;
 import java.awt.*;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 // Grid3x3 class.
 public class Grid3x3 {
     // Declarations and init.
     private static JFrame GUI = new JFrame(); // GUI declaration from JFrame.
     private static Container pane = GUI.getContentPane(); // pane declaration from Container.
-    private static Scanner scanner = new Scanner(System.in); // Scanner object for user input from Scanner.
+    // private static Scanner scanner = new Scanner(System.in); // Scanner object for user input from Scanner.
 
     public static void main(String[] args) {
-        // Give properties to the frame.
-        GUI.setTitle("3x3 Grid");
-        GUI.setSize(300, 300);
-        GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Declaration and init.
+        String ans; // Used for the do while loop.
 
-        // Grabs user input for width and height using option panes.
-        JOptionPane opane = new JOptionPane(JOptionPane.QUESTION_MESSAGE, JOptionPane.QUESTION_MESSAGE);
-        opane.setVisible(true);
-        int w = Integer.parseInt(JOptionPane.showInputDialog("Enter width:", 0));
-        int h = Integer.parseInt(JOptionPane.showInputDialog("Enter hours:", 0));
-        /* System.out.println("Enter width: ");
-        int w = Integer.parseInt(scanner.nextLine());
-        System.out.println("Enter height: ");
-        int h = Integer.parseInt(scanner.nextLine()); */
-        // Set the grid size to the user defined width and height.
-        pane.setLayout(new GridLayout(h,w));
+        do {
+            // Give properties to the frame.
+            GUI.setTitle("3x3 Grid");
+            GUI.setSize(300, 300);
+            GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            pane.removeAll(); // Removes all panes.
 
-        // Loops through the amount of panels that needs to be created.
-        // Panel # is calculated through user inputted wxh.
-        for (int i = 0; i < (w * h); i++) {
-            createPanel();
-        }
+            // Grabs user input for width and height using option panes.
+            JOptionPane opane = new JOptionPane(JOptionPane.QUESTION_MESSAGE, JOptionPane.QUESTION_MESSAGE);
+            opane.setVisible(true);
+            int w = Integer.parseInt(JOptionPane.showInputDialog("Enter width:", 3));
+            int h = Integer.parseInt(JOptionPane.showInputDialog("Enter hours:", 3));
 
-        // Make the GUI visible to the user.
-        GUI.setVisible(true);
+            /* System.out.println("Enter width: ");
+            int w = Integer.parseInt(scanner.nextLine());
+            System.out.println("Enter height: ");
+            int h = Integer.parseInt(scanner.nextLine()); */
+            // Set the grid size to the user defined width and height.
+            pane.setLayout(new GridLayout(h, w));
+
+            // Loops through the amount of panels that needs to be created.
+            // Panel # is calculated through user inputted wxh.
+            for (int i = 0; i < (w * h); i++) {
+                createPanel();
+            }
+
+            // Make the GUI visible to the user.
+            GUI.setVisible(true);
+
+            ans = JOptionPane.showInputDialog("Run again? (y/n):", "y");
+        } while (ans.equals("y") || ans.equals("Y"));
+        GUI.setVisible(false);
+        System.exit(0);
     }
 
     // createPanel
