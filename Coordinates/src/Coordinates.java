39b277d5704bf92cd03934437239f8476c90a169
Jack Margeson <margeson.jack@gmail.com>
2019-08-28 11:52:30 -0400

    JFrame and JPanel init

--- Coordinates/src/Coordinates.java
+++ Coordinates/src/Coordinates.java
@@ -8,16 +8,31 @@ import java.awt.*; // Color.
 
 public class Coordinates {
     // Declarations and init.
-    private static JFrame GUI = new JFrame(); // GUI declaration from JFrame.
-    private static Container pane = GUI.getContentPane(); // pane declaration from Container.
+    private static JFrame frame = new JFrame(); // GUI declaration from JFrame.
+    private static Container pane = frame.getContentPane(); // pane declaration from Container.
 
     // Main.
     public static void main(String[] args) {
         // Give properties to the frame.
-        GUI.setTitle("Coordinates");
-        GUI.setSize(500, 500);
-        GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Coordinates");
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        GUI.setVisible(true);
+        // Initializes panel.
+        initPanel();
+
+        // Make GUI visible to the user.
+        frame.setVisible(true);
+    }
+
+    private static void initPanel() {
+        // Make panel that takes up the entire width and height of the pane.
+        JPanel panel = new JPanel();
+        panel.setSize(500, 500);
+        panel.setBackground(new Color(196, 255, 199));
+        // Set pane layout.
+        pane.setLayout(new GridLayout(1, 1));
+        // Add panel to the pane.
+        pane.add(panel);
     }
 }
