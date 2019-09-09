f2100c636242c6146da59b2fdfa0d1552ed655bd
Jack Margeson <margeson.jack@gmail.com>
2019-09-09 14:23:22 -0400

    Added coordinate grid

--- Coordinates/src/Coordinates.java
+++ Coordinates/src/Coordinates.java
@@ -15,15 +15,18 @@ public class Coordinates{
     private static ColorPanel pane = new ColorPanel(new Color(255, 255, 255)); // Panel from ColorPanel.
 
     // Variables used for drawing labels.
-    private static int x;
-    private static int y;
+    /* These two variables are set to -5 in order to have them not be displayed
+    when paintComponent is called on start up. */
+    private static int x = -5;
+    private static int y = -5;
+    // This string is set to null in order to prevent paintComponent from freaking out.
     private static String cstring = "";
 
+    // Main program.
     public static void main(String[] args) {
         // Set JFrame properties.
         frame.setSize(500, 500);
         // Set ColorPanel properties.
-        pane.setSize(500, 500);
         // Add ColorPanel to frame.
         frame.add(pane);
 
@@ -37,6 +40,7 @@ public class Coordinates{
                 super.mouseClicked(e);
                 x = e.getX();
                 y = e.getY();
+                // Adjusted coordinate string for screen output.
                 cstring = "(" + (((pane.getWidth()/2) - e.getX())*-1) + ", " + ((pane.getHeight()/2) - e.getY()) + ")";
                 pane.repaint();
             }
@@ -45,6 +49,12 @@ public class Coordinates{
 
     // ColorPanel class
     static class ColorPanel extends JPanel {
+        // ColorPanel constructors.
+        // Default constructor.
+        ColorPanel() {
+            setBackground(Color.white);
+        }
+        // Fill constructor.
         ColorPanel(Color color) {
             setBackground(color);
         }
@@ -55,6 +65,17 @@ public class Coordinates{
             // Create Graphics2D component.
             Graphics2D g2 = (Graphics2D) g;
 
+            // Drawing grid first, then axis to overlay.
+
+            // Grid properties.
+            g2.setColor(new Color(206, 206, 206));
+            g2.setStroke(new BasicStroke(1));
+            // Create grid lines.
+            for (int i = 0; i < getWidth(); i+=10) {
+                g2.drawLine(i, 0, i, getHeight());
+                g2.drawLine(0, i, getWidth(), i);
+            }
+
             // Axis properties.
             g2.setColor(Color.black);
             g2.setStroke(new BasicStroke(3));
@@ -62,6 +83,9 @@ public class Coordinates{
             g2.drawLine(getWidth()/2, 0, getWidth()/2, getHeight());
             g2.drawLine(0, getHeight()/2, getWidth(), getHeight()/2);
 
+
+            // Text properties.
+            g.setColor(Color.black);
             // Draw label showing origin.
             g.drawString("(0, 0)", getWidth()/2 + 10, (getHeight()/2) - 5);
             // Draw label showing coordinates.
