eb8882d6da894997c03156dc03eefb60cf83b1c7
Jack Margeson <margeson.jack@gmail.com>
2019-11-24 22:04:46 -0500

    Multiply matrices

--- Matrix/src/Matrix.java
+++ Matrix/src/Matrix.java
@@ -1,2 +1,131 @@
+/* Matrix.java
+Matrix class for Matrix Lab.
+Jack Margeson, 11/11/2019 */
+
+import java.awt.desktop.SystemEventListener;
+
 public class Matrix {
+    // Private data.
+    private int[][] matrix;
+
+    // Constructors.
+    // Default constructor.
+    public Matrix() {
+        this.matrix = new int[2][2];
+    }
+    // Fill constructor.
+    public Matrix(int width, int height) {
+        this.matrix = new int[width][height];
+    }
+
+    // Gets and sets.
+    public int[][] getMatrix() {
+        return matrix;
+    }
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+    public int getWidth() {
+        return matrix.length;
+    }
+    public int getHeight() {
+        return matrix[0].length;
+    }
+    public void setEntry(int width, int height, int i) {
+        this.matrix[width][height] = i;
+    }
+    public int getEntry(int width, int height) {
+        return matrix[width][height];
+    }
+
+    // Member functions.
+    // check(Matrix matrix);
+    // Compares two matrices and returns a boolean based on if they can be operated with addition and subtraction.
+    public boolean check(Matrix matrix) {
+        return this.getWidth() == matrix.getWidth() && this.getHeight() == matrix.getHeight();
+    }
+
+    // checkMultiplication(Matrix matrix);
+    // Compares two matrices and returns a boolean based on if they can be operated with multiplication.
+    public boolean checkMultiplication(Matrix matrix) {
+        return matrix.getHeight() == this.getWidth();
+    }
+
+    // add(Matrix matrix);
+    // Adds one matrix to another and returns the result matrix.
+    public Matrix add(Matrix matrix) {
+        if (check(matrix)) {
+            Matrix sum = new Matrix(this.getWidth(), this.getHeight());
+            for (int i = 0; i < this.getHeight(); i++) {
+                for (int j = 0; j < this.getWidth(); j++) {
+                    sum.setEntry(j-1, i-1, this.getEntry(j, i) + matrix.getEntry(j, i));
+                }
+            }
+            return sum;
+        } else {
+            return null;
+        }
+    }
+
+    // subtract(Matrix matrix);
+    // Subtracts one matrix to another and returns the result matrix.
+    public Matrix subtract(Matrix matrix) {
+        if (check(matrix)) {
+            Matrix result = new Matrix(this.getWidth(), this.getHeight());
+            for (int i = 0; i < this.getHeight(); i++) {
+                for (int j = 0; j < this.getWidth(); j++) {
+                    result.setEntry(j, i, this.getEntry(j, i) - matrix.getEntry(j, i));
+                }
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    // scalar(int scalar);
+    // Multiplies each element in the matrix by the same number (scalar multiplication).
+    public Matrix scalar(int scalar) {
+        Matrix result = new Matrix(this.getWidth(), this.getHeight());
+        for (int i = 0; i < this.getHeight(); i++) {
+            for (int j = 0; j < this.getWidth(); j++) {
+                result.setEntry(j, i, this.getEntry(j, i) * scalar);
+            }
+        }
+        return result;
+    }
+
+    // multiply(Matrix matrix);
+    // Multiplies one matrix to another and returns the result matrix.
+    public Matrix multiply(Matrix matrix) {
+        if (checkMultiplication(matrix)) {
+            Matrix result = new Matrix(matrix.getWidth(), this.getHeight());
+            for (int i = 0; i < result.getHeight(); i++) {
+                for (int j = 0; j < result.getWidth(); j++) {
+                    int value = 0;
+                    for (int k = 0; k < this.getWidth(); k++) {
+                        value += this.getEntry(k, i) * matrix.getEntry(j, k);
+                    }
+                    result.setEntry(j, i, value);
+                }
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    // toString();
+    // Gets matrix in string form.
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < this.getHeight(); i++) {
+            for (int j = 0; j < this.getWidth(); j++) {
+                s.append(this.getEntry(j,i)).append(" ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 }
--- Matrix/src/MatrixTest.java
+++ Matrix/src/MatrixTest.java
@@ -1,2 +1,72 @@
 public class MatrixTest {
+    public static void main(String[] args) {
+        // Addition test.
+        /*// Create three matrices used for calculation.
+        Matrix m1 = new Matrix(3, 3);
+        Matrix m2 = new Matrix(3, 3);
+        Matrix m3 = new Matrix();
+
+        // Create data for matrix 1 and matrix 2.
+        int cc = 0;
+        for (int i = 0; i < m1.getHeight(); i++) {
+            for (int j = 0; j < m1.getWidth(); j++) {
+                cc++;
+                m1.setEntry(j, i, cc);
+                m2.setEntry(j, i, cc);
+            }
+        }
+
+        // Print the values of all of the matrices.
+        System.out.println("Matrix 1:\n" + m1);
+        System.out.println("Matrix 2:\n" + m2);
+        System.out.println("Matrix 3:\n" + m3);
+
+        // Create answer matrix.
+        Matrix answer;
+
+        // Adding matrix 1 and 2 should correctly output the answer matrix.
+        answer = m1.add(m2);
+        System.out.println("Sum of m1 and m2:");
+        if (answer != null) { System.out.println(answer); } else { System.out.println("These matrices cannot be added."); }
+
+        // Adding matrix 2 and 3 should correctly state that the matrices cannot be added.
+        answer = m2.add(m3);
+        System.out.println("Sum of m2 and m3:");
+        if (answer != null) { System.out.println(answer); } else { System.out.println("These matrices cannot be added."); }*/
+
+        // Multiplication test.
+        /* Matrix m1 = new Matrix(4, 2);
+        Matrix m2 = new Matrix(2, 4);
+
+        m1.setEntry(0,0,3);
+        m1.setEntry(1,0,1);
+        m1.setEntry(2,0,8);
+        m1.setEntry(3,0, 5);
+        m1.setEntry(0,1,3);
+        m1.setEntry(1,1,9);
+        m1.setEntry(2,1,4);
+        m1.setEntry(3,1, 6);
+
+        m2.setEntry(0 ,0, 4);
+        m2.setEntry(1 ,0, 3);
+        m2.setEntry(0 ,1, 2);
+        m2.setEntry(1 ,1, 5);
+        m2.setEntry(0 ,2, 2);
+        m2.setEntry(1 ,2, 8);
+        m2.setEntry(0 ,3, 3);
+        m2.setEntry(1 ,3, 5);
+
+        System.out.println(m1);
+        System.out.println(m2);
+
+        Matrix m3 = new Matrix();
+
+        m3 = m1.multiply(m2);
+
+        System.out.println(m3);*/
+
+        // Init Swing.
+        
+
+    }
 }
